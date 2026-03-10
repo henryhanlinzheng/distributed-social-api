634541ad46087158dc29bf6abc629a7178d3a2a3
Henry Hanlin Zheng <hhzheng1@uci.edu>
2026-03-09 21:50:03 -0700

    Fetch data from api request and catch three custom exceptions

--- WebAPI.py
+++ WebAPI.py
@@ -16,21 +16,35 @@ from abc import ABC, abstractmethod
 
 class WebAPI(ABC):
 
-  def __init__(self):
-    self.set_apikey = None
+	def __init__(self):
+		self.set_apikey = None
 
-  def _download_url(self, url: str) -> dict:
-    #TODO: Implement web api request code in a way that supports
-    # all types of web APIs
-    pass
+	def _download_url(self, url: str) -> dict:
+		#TODO: Implement web api request code in a way that supports
+		# all types of web APIs
+		response = None
+		try:
+			response = urllib.request.urlopen(url)
+			data = response.read()
+			return json.loads(data)
 
-  def set_apikey(self, apikey:str) -> None:
-    pass
+		except urllib.error.HTTPError as e:
+				raise Exception(f"Remote API unavailable (HTTP Error {e.code})") from e
+		except urllib.error.URLError as e:
+				raise Exception("Loss of local connection to the Internet") from e
+		except json.JSONDecodeError as e:
+				raise Exception("Invalid data formatting from the remote API") from e
+		finally:
+			if response is not None:
+				response.close()
 
-  @abstractmethod
-  def load_data(self):
-    pass
+	def set_apikey(self, apikey:str) -> None:
+		pass
 
-  @abstractmethod
-  def transclude(self, message:str) -> str:
-    pass
+	@abstractmethod
+	def load_data(self):
+		pass
+
+	@abstractmethod
+	def transclude(self, message:str) -> str:
+		pass
