81d7c6b11bf6de85282eb9f3853b44d650c277bd
Henry Hanlin Zheng <hhzheng1@uci.edu>
2026-03-09 20:31:30 -0700

    Initialize variables for OpenWeather

--- OpenWeather.py
+++ OpenWeather.py
@@ -8,3 +8,35 @@
 # Henry Hanlin Zheng
 # hhzheng1@uci.edu
 # 19204536
+
+class OpenWeather:
+    def __init__(self, zipcode="92612", ccode="US") -> None:
+        self.zipcode = zipcode
+        self.ccode = ccode
+        self._apikey = None
+        self.temperature = None
+        self.high_temp = None
+        self.low_temp = None
+        self.description = None
+        self.humidity = None
+        self.city = None
+        self.sunset=None
+
+
+    def set_apikey(self, apikey:str) -> None:
+        '''
+        Sets the apikey required to make requests to a web API.
+        :param apikey: The apikey supplied by the API service
+        '''
+        #TODO: assign apikey value to a class data attribute that can be accessed by class members
+        pass
+
+    def load_data(self) -> None:
+        '''
+        Calls the web api using the required values and stores the response in class data attributes.
+        '''
+        #TODO: use the apikey data attribute and the urllib module to request data from the web api. See sample code at the begining of Part 1 for a hint.
+        #TODO: assign the necessary response data to the required class data attributes
+        pass
+        
+    
