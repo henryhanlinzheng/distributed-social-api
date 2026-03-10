9fdd1ce9175bf4698abd5458a0687ba39b1e9094
Henry Hanlin Zheng <hhzheng1@uci.edu>
2026-03-09 20:37:43 -0700

    Initialize apikey and url variables

--- OpenWeather.py
+++ OpenWeather.py
@@ -29,13 +29,14 @@ class OpenWeather:
         :param apikey: The apikey supplied by the API service
         '''
         #TODO: assign apikey value to a class data attribute that can be accessed by class members
-        pass
+        self._apikey = apikey
 
     def load_data(self) -> None:
         '''
         Calls the web api using the required values and stores the response in class data attributes.
         '''
         #TODO: use the apikey data attribute and the urllib module to request data from the web api. See sample code at the begining of Part 1 for a hint.
+        url = f"http://api.openweathermap.org/data/2.5/weather?zip={self.zipcode},{self.ccode}&appid={self._apikey}"
         #TODO: assign the necessary response data to the required class data attributes
         pass
         
